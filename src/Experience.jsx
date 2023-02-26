aabedf5925128e2d80bc5c15f5f7fbb550c8a1a5
Daniel Baramo <96897948+dbaramo@users.noreply.github.com>
2023-02-26 14:08:54 -0500

    added camera animation

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,3 @@
-import { OrbitControls } from "@react-three/drei";
 import { Physics, Debug } from "@react-three/rapier";
 import Lights from "./Lights.jsx";
 import { Level } from "./Level.jsx";
@@ -7,10 +6,8 @@ import Player from "./Player.jsx";
 export default function Experience() {
   return (
     <>
-      <OrbitControls makeDefault />
-
       <Physics>
-        <Debug />
+        {/* <Debug /> */} 
         <Lights />
         <Level />
         <Player />
